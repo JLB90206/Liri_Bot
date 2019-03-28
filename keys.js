8cc2d85dce70ca26bf9839078cf2022d242299fa
Jordan Babbitt <JBabbitt27@gmail.com>
2019-03-28 12:07:22 -0700

    Partial working code

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('!!Spotify API LOADED!!');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+}; 
+
+console.log('!!TMDB API LOADED!!')
+
+exports.tmdbKey = '800bcceeffd6ab16dc5c957a2f09a8b1'
